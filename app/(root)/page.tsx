dc8b4810a467cf707744c748e89938e842a6c516
karannkushwaha <karankushwaha7227@gmail.com>
2025-01-26 12:39:49 +0530

    shadcn setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,24 @@
-export default function Home() {
+import SearchForm from "../../components/SearchForm";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
   return (
     <>
-      <h1 className="text-2xl">home</h1>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pich your startup
+          <br />
+          connect with entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Notice in Virtual Competitions.
+        </p>
+        <SearchForm query={query} />
+      </section>
     </>
   );
 }
